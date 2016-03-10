08a53e2a2077b01c0546545992220b550940ebbf
Andrew Bayer <andrew.bayer@gmail.com>
2016-03-10 12:49:38 -0800

    Initial config entry point, stub of step classes

---
+++ src/main/groovy/org/jenkinsci/plugins/plumber/PlumberStep.groovy
@@ -0,0 +1,65 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2016, CloudBees, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.jenkinsci.plugins.plumber
+
+import hudson.Extension
+import org.jenkinsci.plugins.workflow.steps.AbstractStepDescriptorImpl
+import org.jenkinsci.plugins.workflow.steps.AbstractStepImpl
+import org.kohsuke.stapler.DataBoundConstructor
+
+
+public class PlumberStep extends AbstractStepImpl {
+    private final Closure input
+
+    @DataBoundConstructor
+    public PlumberStep(Closure input) {
+        this.input = input
+    }
+
+    @Override
+    public DescriptorImpl getDescriptor() {
+        return (DescriptorImpl)super.getDescriptor();
+    }
+
+    @Extension
+    public static class DescriptorImpl extends AbstractStepDescriptorImpl {
+
+        public DescriptorImpl() {
+            super(PlumberStepExecution.class);
+        }
+
+        @Override
+        public String getFunctionName() {
+            return "plumber";
+        }
+
+        @Override
+        public String getDisplayName() {
+            return "Run Plumber";
+        }
+    }
+
+
+
+}
