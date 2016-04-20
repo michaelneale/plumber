54043426b69e45a0b799d6082c205bb77f64ee77
Andrew Bayer <andrew.bayer@gmail.com>
2016-04-20 10:41:24 +0200

    Adding a more complicated CPS closure test

--- src/test/java/org/jenkinsci/plugins/plumber/PlumberStepDSLTest.java
+++ src/test/java/org/jenkinsci/plugins/plumber/PlumberStepDSLTest.java
@@ -334,6 +334,27 @@ public class PlumberStepDSLTest {
 
     }
 
+    @Test
+    public void testStashUnstashToDirectoryCPSClosure() throws Exception {
+        sampleRepo.init();
+        sampleRepo.write("Jenkinsfile",
+                pipelineSourceFromResources("stashUnstashToDirectoryCPSClosure"));
+
+        sampleRepo.git("add", "Jenkinsfile");
+        sampleRepo.git("commit", "--message=files");
+        story.addStep(new Statement() {
+            @Override public void evaluate() throws Throwable {
+                WorkflowJob p = story.j.jenkins.createProject(WorkflowJob.class, "p");
+                p.setDefinition(new CpsScmFlowDefinition(new GitStep(sampleRepo.toString()).createSCM(), "Jenkinsfile"));
+                WorkflowRun b = p.scheduleBuild2(0).waitForStart();
+                story.j.assertLogContains("outputDir",
+                        story.j.assertBuildStatusSuccess(story.j.waitForCompletion(b)));
+                story.j.assertLogContains("PANTStrousers", b);
+            }
+        });
+    }
+
+
     private String pipelineSourceFromResources(String pipelineName) throws IOException {
         String pipelineSource = null;
 
